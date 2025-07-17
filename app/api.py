6bc15ea02f22460050728334f8a9879a433ce66e
Iasmina <iacobiasmina@yahoo.com>
2025-07-18 01:15:29 +0300

    local frontend

--- app/api.py
+++ app/api.py
@@ -15,10 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000", 
-        "https://resume-analyzer-psi.vercel.app", 
-    ],
+    allow_origins=["http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
